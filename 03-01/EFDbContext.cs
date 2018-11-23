ef72396db2bbadcc82af58677bd178480f425731
dafeng2018 <wf011012@163.com>
2018-11-23 16:49:44 +0800

    commit

---
+++ 03-01/EFDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace _03_01
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext() : base("name=ConnectionString")
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<EFDbContext>());
+        }
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
