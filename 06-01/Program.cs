d03b510ad3d4461a0d52196310a9346f3419aeef
dafeng2018 <wf011012@163.com>
2018-12-27 17:22:02 +0800

    commit

--- 06-01/Program.cs
+++ 06-01/Program.cs
@@ -96,47 +96,126 @@ namespace _06_01
             //}
             #endregion
             #region 04 - 客户端获胜 
-            using (var ctx = new EFDbContext())
-            {
-                //ctx.Database.Log = Console.WriteLine;
-                using (var ctx1 = new EFDbContext())
-                {
-                    //ctx1.Database.Log = Console.WriteLine;
-                    var id = 1;
-                    var s1 = ctx.Students.FirstOrDefault(d => d.Id == id);
-                    var s2 = ctx1.Students.FirstOrDefault(d => d.Id == id);
-                    s1.Name = "dafeng - 1";
-                    ctx.SaveChanges();
+            //using (var ctx = new EFDbContext())
+            //{
+            //    //ctx.Database.Log = Console.WriteLine;
+            //    using (var ctx1 = new EFDbContext())
+            //    {
+            //        //ctx1.Database.Log = Console.WriteLine;
+            //        var id = 1;
+            //        var s1 = ctx.Students.FirstOrDefault(d => d.Id == id);
+            //        var s2 = ctx1.Students.FirstOrDefault(d => d.Id == id);
+            //        s1.Name = "dafeng - 1";
+            //        ctx.SaveChanges();
 
-                    s2.Name = "dafeng - 2";
-                    try
-                    {
-                        ctx1.SaveChanges();
-                    }
-                    catch (DbUpdateConcurrencyException ex)
-                    {
-                        var tracking = ex.Entries.Single();
-                        var original = (Student)tracking.OriginalValues.ToObject();
-                        var database = (Student)tracking.GetDatabaseValues().ToObject();
-                        var current = (Student)tracking.CurrentValues.ToObject();
+            //        s2.Name = "dafeng - 2";
+            //        try
+            //        {
+            //            ctx1.SaveChanges();
+            //        }
+            //        catch (DbUpdateConcurrencyException ex)
+            //        {
+            //            var tracking = ex.Entries.Single();
+            //            var original = (Student)tracking.OriginalValues.ToObject();
+            //            var database = (Student)tracking.GetDatabaseValues().ToObject();
+            //            var current = (Student)tracking.CurrentValues.ToObject();
 
 
-                        Console.WriteLine("原始值({" + original.Id + "},{" + original.Name + "},{" + original.Age + "})");
-                        Console.WriteLine("-----------------------------");
-                        //var dbValues = @"数据库值：({database.Id},{database.Name},{database.Age})";
-                        Console.WriteLine("数据库值：({" + database.Id + "},{ " + database.Name + "},{ " + database.Age + "})");
-                        Console.WriteLine("-----------------------------");
-                        //var update = $"更新值：({current.Id},{current.Name},{current.Age})";
-                        Console.WriteLine("更新值：({ " + current.Id + "},{" + current.Name + "},{" + current.Age + "})");
-                        Console.WriteLine("-----------------------------");
-                        tracking.OriginalValues.SetValues(database);
-                        ctx1.SaveChanges();
-                    }
-                }
-            }
+            //            Console.WriteLine("原始值({" + original.Id + "},{" + original.Name + "},{" + original.Age + "})");
+            //            Console.WriteLine("-----------------------------");
+            //            //var dbValues = @"数据库值：({database.Id},{database.Name},{database.Age})";
+            //            Console.WriteLine("数据库值：({" + database.Id + "},{ " + database.Name + "},{ " + database.Age + "})");
+            //            Console.WriteLine("-----------------------------");
+            //            //var update = $"更新值：({current.Id},{current.Name},{current.Age})";
+            //            Console.WriteLine("更新值：({ " + current.Id + "},{" + current.Name + "},{" + current.Age + "})");
+            //            Console.WriteLine("-----------------------------");
+            //            tracking.OriginalValues.SetValues(database);
+            //            ctx1.SaveChanges();
+            //        }
+            //    }
+            //}
+            #endregion
+            #region 05 - 数据库获胜 
+            //using (var ctx = new EFDbContext())
+            //{
+            //    //ctx.Database.Log = Console.WriteLine;
+            //    using (var ctx1 = new EFDbContext())
+            //    {
+            //        //ctx1.Database.Log = Console.WriteLine;
+            //        var id = 1;
+            //        var s1 = ctx.Students.FirstOrDefault(d => d.Id == id);
+            //        var s2 = ctx1.Students.FirstOrDefault(d => d.Id == id);
+            //        s1.Name = "dafeng - 1";
+            //        ctx.SaveChanges();
+
+            //        s2.Name = "dafeng - 2";
+            //        try
+            //        {
+            //            ctx1.SaveChanges();
+            //        }
+            //        catch (DbUpdateConcurrencyException ex)
+            //        {
+            //            return;
+            //        }
+            //    }
+            //}
+            #endregion
+            #region 06 - 客户端和数据库合并获胜 https://github.com/striveCj/StudyCode
+            //using (var ctx = new EFDbContext())
+            //{
+            //    //ctx.Database.Log = Console.WriteLine;
+            //    using (var ctx1 = new EFDbContext())
+            //    {
+            //        //ctx1.Database.Log = Console.WriteLine;
+            //        var id = 1;
+            //        var s1 = ctx.Students.FirstOrDefault(d => d.Id == id);
+            //        var s2 = ctx1.Students.FirstOrDefault(d => d.Id == id);
+            //        s1.Name = "dafeng - 1";
+            //        s1.Age = 25;
+            //        ctx.SaveChanges();
+
+            //        s2.Name = "dafeng - 2";
+            //        s2.Age = 26;
+            //        try
+            //        {
+            //            ctx1.SaveChanges();
+            //        }
+            //        catch (DbUpdateConcurrencyException ex)
+            //        {
+            //            Retry(ctx1, handleDbUpdateConcurrencyException: exception =>
+            //            {
+            //                exception = (ex as DbUpdateConcurrencyException).Entries;
+            //                var tracking = exception.Single();
+            //                var databaseValues = tracking.GetDatabaseValues();
+            //                var originalValues = tracking.OriginalValues.Clone();
+            //                var original = (Student)tracking.OriginalValues.ToObject();
+            //                var database = (Student)tracking.GetDatabaseValues().ToObject();
+            //                var current = (Student)tracking.CurrentValues.ToObject();
+
+            //                tracking.OriginalValues.SetValues(databaseValues);
+            //                databaseValues.PropertyNames.Where(property => !object.Equals(originalValues[property], databaseValues[property])).ToList().ForEach(property => tracking.Property(property).IsModified = false);
+            //            });
+            //        }
+            //    }
+            //}
             #endregion
 
-
         }
+        static int Retry(EFDbContext context, Action<IEnumerable<DbEntityEntry>> handleDbUpdateConcurrencyException, int retryCount = 3)
+        {
+            for (int retry = 1; retry < retryCount; retry++)
+            {
+                try
+                {
+                    return context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException exception)
+                {
+                    handleDbUpdateConcurrencyException(exception.Entries);
+                }
+            }
+            return context.SaveChanges();
+        }
+
     }
 }
---
+++ EFUnitOfWork/EF.Data/Mapping.cs
@@ -0,0 +1,31 @@
+﻿using EF.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Data
+{
+    public class BookMap : EntityTypeConfiguration<Book>
+    {
+        public BookMap()
+        {
+            //table
+            ToTable("Books");
+
+            //key
+            HasKey(t => t.ID);
+
+            //property
+            Property(t => t.ID).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(t => t.Title).IsRequired();
+            Property(t => t.Author).IsRequired();
+            Property(t => t.ISBN).IsRequired();
+            Property(t => t.Published).IsRequired();
+            Property(t => t.Url).IsRequired();
+        }
+    }
+}
