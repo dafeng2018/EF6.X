423a65ac7f360df78e3c523b671a1171c282c0d4
dafeng2018 <wf011012@163.com>
2019-03-04 17:37:30 +0800

    commit

---
+++ 08-01-01/EF.Data/Mapping/BookMap.cs
@@ -0,0 +1,32 @@
+﻿using EF.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Data.Mapping
+{
+    public class BookMap : EntityTypeConfiguration<Book>
+    {
+        public BookMap()
+        {
+            //table
+            ToTable("Books");
+
+            //key
+            HasKey(t => t.ID);
+
+            //property
+            Property(t => t.ID).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(t => t.Title).IsRequired().HasMaxLength(200);
+            Property(t => t.Author).IsRequired().HasMaxLength(200);
+            Property(t => t.ISBN).IsRequired().HasMaxLength(200);
+            Property(t => t.Published).IsRequired();
+            Property(t => t.Url).IsRequired();
+        }
+
+    }
+}
---
+++ 08-01-01/Ef.WebSite/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ef.WebSite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ef.WebSite")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8a98b8c4-be50-4837-bae4-adf38c5c6dc8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
